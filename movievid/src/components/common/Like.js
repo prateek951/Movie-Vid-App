71a99840161de722acc1c7ce89568e13140e0974
prateek951 <prince.cenation@gmail.com>
2018-09-25 14:44:47 +0530

    Likecomponent works and converted to stateless

--- movievid/src/components/common/Like.js
+++ movievid/src/components/common/Like.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-class Like extends Component {
-    state = { liked: true }
-    render() { 
-        console.log('inside the Like',this.props);
-        const { liked } = this.props;
-        let classes = 'fa fa-heart';
-        if(!liked) {
-            classes += '-o';
-        }
-        return (<i onClick={this.props.onClick} className={classes} aria-hidden="true"></i>)
-    }
-}
- 
+import React from "react";
+
+const Like = ({ liked, onClick }) => {
+  let classes = "fa fa-heart";
+  if (!liked) {
+    classes += "-o";
+  }
+  return (
+      <i className={classes} onClick={onClick} style={{cursor: "pointer"}}
+      aria-hidden="true"></i>
+  );
+};
+
 export default Like;
