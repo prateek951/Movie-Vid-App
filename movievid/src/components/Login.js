471c3b781506805dfe3a544c85f3793d6f97aa60
prateek951 <prince.cenation@gmail.com>
2018-09-26 14:07:39 +0530

    validation more specific

--- movievid/src/components/Login.js
+++ movievid/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from 'joi-browser';
 import Input from "./common/Input";
 
 export default class Login extends Component {
@@ -10,22 +11,44 @@ export default class Login extends Component {
     errors: {}
   };
 
-  validate = ({username,password}) => {
+  //Define the Joi Schema 
+  schema = {
+    username : Joi.string().required().label('Username'),
+    password : Joi.string().required().label('Password')
+  };
+  //Joi sophisticated validation
+    validate = ({username,password}) => {
+      const options = { abortEarly : false };
+      const { error } = Joi.validate({username,password},this.schema,options);
+      if(!error) {
+        return null;
+      }
+      const errors = {};
+      for (let item of error.details) {
+          errors[item.path[0]] = item.message;
+      }
+      console.log(errors);
+      return errors;
+    }    
   
-    //Basic Client Side validation 
+
+  // Basic clientside validation
+  // validate = ({username,password}) => {
+    
+  //   //Basic Client Side validation 
   
-    const errors = {};
+  //   const errors = {};
 
-    if(username.trim() === '') {
-      errors.username = 'Username is required';
-    }
+  //   if(username.trim() === '') {
+  //     errors.username = result.error.details[0].message;
+  //   }
 
-    if(password.trim() === '') {
-      errors.password = 'Password is required';
-    }
+  //   if(password.trim() === '') {
+  //     errors.password = result.error.details[1].message;
+  //   }
 
-    return Object.keys(errors).length === 0 ? null : errors;
-  }
+  //   return Object.keys(errors).length === 0 ? null : errors;
+  // }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -35,31 +58,37 @@ export default class Login extends Component {
     this.setState({ errors: errors || {} });
     if(errors) return;
   }
-  validateProperty = ({name,value}) => {
-    if(name === 'username') {
-      if(value.trim() === '') {
-        return 'Username is required';
-      } 
-    }
-    if(name === 'password') {
-      if(value.trim() === '') {
-        return 'Password is required';
-      }
-    }
+  handleStringChange = e => {
+    const errors = this.validate(this.state.account);
+    this.setState({ ...this.state.account, account : { [e.target.name] : [e.target.value], errors: errors || {} }});
   }
+  // validateProperty = ({name,value}) => {
+  //   if(name === 'username') {
+  //     if(value.trim() === '') {
+  //       return 'Username is required';
+  //     } 
+  //   }
+  //   if(name === 'password') {
+  //     if(value.trim() === '') {
+  //       return 'Password is required';
+  //     }
+  //   }
+  // }
+  // Code for the basic form validation
 
-  handleStringChange = ({currentTarget:input}) => {
-    const errors = Object.assign({},this.state.errors);
-    const errorMessage = this.validateProperty(input);
-    if(errorMessage) {
-      errors[input.name] = errorMessage;
-    }else {
-      delete errors[input.name];
-    }
-    const account = { ...this.state.account }; 
-    account[input.name] = input.value;
-    this.setState({ account: account, errors });
-  };
+  // handleStringChange = ({currentTarget:input}) => {
+  //   const errors = Object.assign({},this.state.errors);
+  //   const errorMessage = this.validateProperty(input);
+  //   if(errorMessage) {
+  //     errors[input.name] = errorMessage;
+  //   }else {
+  //     delete errors[input.name];
+  //   }
+  //   const account = { ...this.state.account }; 
+  //   account[input.name] = input.value;
+  //   this.setState({ account: account, errors });
+  // };
+
   // renderErrors = () => {
   //   const { errors } = this.state;
   //   const myErrors = [];
