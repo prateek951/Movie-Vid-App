79c7acf2681f2a1d8ea656eb1716895c4e0b99a3
prateek951 <prince.cenation@gmail.com>
2018-09-29 18:03:52 +0530

    setting env variables

--- movievid/src/index.js
+++ movievid/src/index.js
@@ -7,6 +7,9 @@ import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+
+console.log('Superman',process.env.REACT_APP_NAME);
+
 ReactDOM.render(
   <Router>
     <App />
