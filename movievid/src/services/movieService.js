f86edf3b45d9ff966b00d15878dc7a96f6e200b1
prateek951 <prince.cenation@gmail.com>
2018-09-29 12:38:28 +0530

    refactored the code

--- movievid/src/services/movieService.js
+++ movievid/src/services/movieService.js
@@ -1,17 +1,21 @@
 import http from './httpService';
 import config from '../config.json';
 
+
 export function getMovies() {
     return http.get(config.endpoint + '/movies');;
 }
+
 
 export function getMovie(movieId) {
     return http.get(config.endpoint + '/movies/' + movieId);
 }
 
+
 export function deleteMovie(movieId) {
     return http.delete(config.endpoint + '/movies/' + movieId);
 }
+
 
 export function saveMovie(movie) {
     if(movie._id) {
